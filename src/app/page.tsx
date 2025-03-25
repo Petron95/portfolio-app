cf9285de36136bcf06019f8a54364da2dc3677f5
Petra Olsson <petraolsson1995@gmail.com>
2025-03-25 15:21:23 +0100

    Added header to startpage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>Petras Portfolio</h1>
         <ol>
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
